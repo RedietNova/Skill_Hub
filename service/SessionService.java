9054c2c62036b2c709a5fd36d5ef7cfb9ce42043
obsinanmohammed-cpu <obsinan.mohammed@bitscollege.edu.et>
2026-06-24 19:08:57 +0300

    added SessionService

---
+++ service/SessionService.java
@@ -0,0 +1,18 @@
+package Skill_Hub.service;
+import database.SessionDAO;
+import model.Session;
+
+public class SessionService {
+
+    private final SessionDAO sessionDAO = new SessionDAO();
+
+    public void createSession(Session session) {
+        // Business rule: validation check
+        if (session.getMentor() == null || session.getLearner() == null || session.getSkill() == null) {
+            System.out.println("Error: Session must contain a valid Mentor, Learner, and Skill.");
+            return;
+        }
+        
+        sessionDAO.insertSession(session);
+    }
+}
